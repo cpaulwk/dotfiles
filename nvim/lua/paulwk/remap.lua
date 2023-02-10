c193d7934fcea20558830c6677dd1cd511a0a96d
Paul Chow <cpaulwenkai@gmail.com>
2023-02-10 13:33:59 +0100

    remap fugitive <leader>e key to --preset floating for coc-explorer

--- nvim/lua/paulwk/remap.lua
+++ nvim/lua/paulwk/remap.lua
@@ -1,26 +1,26 @@
 vim.g.mapleader = " "
 
 -- Quick fixes
-vim.keymap.set("n", "n", "nzz")                                               
-vim.keymap.set("n", "N", "Nzz")                                               
-vim.keymap.set("n", "Y",  "y$")                                                
-vim.keymap.set("n", "L", "Ljjj")                                              
-vim.keymap.set("n", "H", "Hkkk")                                              
-vim.keymap.set("n", "=G", "=Gzz")                                             
-vim.keymap.set("n", "<C-d>", "<C-d>zz")                                       
-vim.keymap.set("n", "<C-u>", "<C-u>zz")                                       
-vim.keymap.set("n", "<C-w>>", "<C-w>100>")                                    
-vim.keymap.set("n", "<C-w><", "<C-w>100<")                                    
-vim.keymap.set("n", "<CR>", "<Cmd>b#<CR>")                                    
-vim.keymap.set("n", "<C-j>", "i<CR><Esc>")                                    
-vim.keymap.set("n", "<C-l>", "<Cmd>bn<CR>")                                   
-vim.keymap.set("n", "<C-h>", "<Cmd>bp<CR>")                                   
-vim.keymap.set("n", "<C-k>", "<Cmd>bn<bar>bd#<CR>")                            
-     
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+vim.keymap.set("n", "Y", "y$")
+vim.keymap.set("n", "L", "Ljjj")
+vim.keymap.set("n", "H", "Hkkk")
+vim.keymap.set("n", "=G", "=Gzz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-w>>", "<C-w>100>")
+vim.keymap.set("n", "<C-w><", "<C-w>100<")
+vim.keymap.set("n", "<CR>", "<Cmd>b#<CR>")
+vim.keymap.set("n", "<C-j>", "i<CR><Esc>")
+vim.keymap.set("n", "<C-l>", "<Cmd>bn<CR>")
+vim.keymap.set("n", "<C-h>", "<Cmd>bp<CR>")
+vim.keymap.set("n", "<C-k>", "<Cmd>bn<bar>bd#<CR>")
+
 
 -- Pick lines and move them Up or Down
-vim.keymap.set("x", "J", ":m '>+1<CR>gv=gv")                                  
-vim.keymap.set("x", "K", ":m '<-2<CR>gv=gv")                                  
+vim.keymap.set("x", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("x", "K", ":m '<-2<CR>gv=gv")
 
 -- Paste and rewrite from the same expression
 vim.keymap.set("x", "<leader>p", "\"_dP")
@@ -35,13 +35,13 @@ vim.keymap.set("n", "<leader>Y", "\"*Y")
 --vim.keymap.set("v", "<leader>d", "\"_d")
 
 -- Fugitive Remap
-vim.keymap.set("n", "<leader>g", "<Cmd>vertical Git<bar>%bd!<bar>b#<CR>")     
+vim.keymap.set("n", "<leader>g", "<Cmd>vertical Git<bar>%bd!<bar>b#<CR>")
 
 --Coc Explorer Remap
-vim.keymap.set("n", "<leader>e", "<Cmd>CocCommand explorer<CR>")              
+vim.keymap.set("n", "<leader>e", "<Cmd>CocCommand explorer --preset floating<CR>")
 
 -- Blamer Remap
-vim.keymap.set("n", "<leader>b", "<Cmd>BlamerToggle<CR>")                     
-    --
+vim.keymap.set("n", "<leader>b", "<Cmd>BlamerToggle<CR>")
+--
 --Gvdiff Remap
-vim.keymap.set("n", "<leader>d", "<Cmd>Gvdiff<CR>")                           
+vim.keymap.set("n", "<leader>d", "<Cmd>Gvdiff<CR>")
